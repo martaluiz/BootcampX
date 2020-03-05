9e325e9df046291d13c9a5233544dd6c631450ed
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-03-05 01:57:09 +0000

    SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,23 +1,28 @@
 const { Pool } = require('pg');
+ 
+const queryString = `select distinct teachers.name as teacher, cohorts.name as cohort
+from teachers
+join assistance_requests on teacher_id = teachers.id
+join students on student_id = students.id 
+join cohorts on cohort_id = cohorts.id
+where cohorts.name = '${process.argv[2] || 'JUL02'}' 
+order by teacher;`
+
+const cohortName = process.argv[2];
+const limit = process.argv[3]|| 5;
+const values = [`%${cohortName}%`, limit];
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+    user: 'vagrant',
+    password: '123',
+    host: 'localhost',
+    database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+pool.query(queryString,values)
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+    res.rows.forEach(row => {
+        console.log(`${row.cohort}: ${row.teacher}`);
+    })
+})
+.catch(err => console.error(`query error`, err.stack));
